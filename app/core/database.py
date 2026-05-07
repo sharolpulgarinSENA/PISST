a2f87ba5b5bcded355553848d88cb1cfec9a8b94
Barner Acosta <barner.acosta@gmail.com>
2026-05-07 09:49:33 -0500

    feat: conexión a Neon configurada

---
+++ app/core/database.py
@@ -0,0 +1,31 @@
+# app/core/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # carga las variables del archivo .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# pool_pre_ping=True → verifica la conexión antes de usarla
+# pool_recycle=300  → renueva conexiones cada 5 minutos
+# sslmode=require   → Neon siempre requiere SSL
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    connect_args={"sslmode": "require"}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
